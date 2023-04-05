ff1b6469b65b4335e6e696049c863c683a458ac1
lostguide <chen.8106@osu.edu>
2023-04-05 17:50:56 -0400

    saving

---
+++ Black.Zinc.Spitz.Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Black.Zinc.Spitz.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Black.Zinc.Spitz.Data
+{
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions<StoreContext> options)
+        : base(options)
+    { }
+
+    public DbSet<Item> Items { get; set; } 
+ 
+}
+}
