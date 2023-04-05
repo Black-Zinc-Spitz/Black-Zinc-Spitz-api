b2ae732654c7dc1856ef00163308a7d312bf4ab3
Barbara Lin <linbarbaralulu@gmail.com>
2023-04-05 13:41:18 -0400

    added back first get; removed package reference

--- Black.Zinc.Spitz.Api/Catalog/CatalogController.cs
+++ Black.Zinc.Spitz.Api/Catalog/CatalogController.cs
@@ -8,6 +8,21 @@ namespace Black.Zinc.Spitz.Api.Controllers
     public class CatalogController : ControllerBase
     {
 
+         //added old version back in after looking at the examples
+         [HttpGet]
+         public IActionResult GetItems()
+         {
+             var items = new List<Item>()
+             {
+               new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+               new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+             };
+    
+             return Ok(items);
+         }
+         //end of alteration
+         
+         
          // step 3 not sure where it goes or if it ovverides the old version so i will add it here
          [HttpGet("{id:int}")]
          public IActionResult GetItem(int id)
