3f55007516081160a89181770d9b5183625ca48a
lostguide <chen.8106@osu.edu>
2023-04-05 17:29:20 -0400

    do not merge!!!

---
+++ Black.Zinc.Spitz.Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Black.Zinc.Spitz.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Black.Zinc.Spitz.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+        : base(options)
+        {}
+
+        public DbSet<InvalidTimeZoneException> Items { get; set; }
+    }
+}
